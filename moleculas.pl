ca8c29c9d0cc5c14b5222aaa48f51fa984b9506e
filippinheiro <filippinheiro1@gmail.com>
2018-11-30 11:58:18 -0200

    Inicio da criação dos predicados

--- moleculas.pl
+++ moleculas.pl
@@ -1,34 +1,35 @@
-%Átomos da Molécula m1
-atomo(m1,h1,hidrogenio).
-atomo(m1,h2,hidrogenio).
-atomo(m1,h3,hidrogenio).
-atomo(m1,h4,hidrogenio).
-atomo(m1,h5,hidrogenio).
-atomo(m1,h6,hidrogenio).
-atomo(m1,h7,hidrogenio).
-atomo(m1,c1,carbono).
-atomo(m1,c2,carbono).
-atomo(m1,c3,carbono).
-atomo(m1,c4,carbono).
-atomo(m1,c5,carbono).
-atomo(m1,c6,carbono).
-atomo(m1,c7,carbono).
-atomo(m1,ch,cloro).
+%%prolog
 
-%Ligações Químicas da Molécula m1
-ligacaosimples(m1,h1,c1).
-ligacaosimples(m1,h2,c3).
-ligacaosimples(m1,h3,c3).
-ligacaosimples(m1,h4,c5).
-ligacaosimples(m1,h5,c3).
-ligacaosimples(m1,h6,c6).
-ligacaosimples(m1,h7,c7).
-ligacaosimples(m1,c1,c2).
-ligacaosimples(m1,c2,ch).
-ligacaosimples(m1,c3,c4).
-ligacaosimples(m1,c4,c6).
-ligacaosimples(m1,c5,c7).
+%%Acetona
+%átomos
+atomo(acetona, h1, hidrogenio).
+atomo(acetona, h2, hidrogenio).
+atomo(acetona, h3, hidrogenio).
+atomo(acetona, h4, hidrogenio).
+atomo(acetona, h5, hidrogenio).
+atomo(acetona, h6, hidrogenio).
+atomo(acetona, c, carbono).
+atomo(acetona, c1, carbono).
+atomo(acetona, c2, carbono).
+atomo(acetona, o1, oxigenio).
 
-ligacaodupla(m1,c6,c7).
-ligacaodupla(m1,c1,c4).
-ligacaodupla(m1,c2,c5).
+%ligações
+ligacaosimples(acetona, h1, c).
+ligacaosimples(acetona, c, c1).
+ligacaosimples(acetona, c1, c2).
+ligacaosimples(acetona, c, h2).
+ligacaosimples(acetona, c, h3).
+ligacaosimples(acetona, c1, o1).
+ligacaosimples(acetona, c2, h4).
+ligacaosimples(acetona, c2, h5).
+ligacaosimples(acetona, c2, h6).
+
+ligacaodupla(acetona, c1, o1).
+
+%idMolecula(M) :- acido(M).
+
+acido(M) :- (ligacaodupla(M, C, O), ligacaosimples(M, C, O1), ligacaosimples(M, O1, H)), (atomo(M, O1, oxigenio), atomo(M, O, oxigenio), atomo(M, C, carbono), atomo(M, H, hidrogenio)), O \= O1, !. 
+
+aldeido(M) :- (ligacaosimples(M, C, C1), ligacaosimples(M, C1, H), ligacaodupla(M, C1, O)), (atomo(M, C, carbono), atomo(M, C1, carbono), atomo(M, O, oxigenio), atomo(M, H, hidrogenio)), C\=C1, !.
+
+cetona(M) :- (ligacaosimples(M, C, C1), ligacaodupla(M, C1, O), ligacaosimples(M, C1, C2)), (atomo(M, C, carbono), atomo(M, C1, carbono), atomo(M, C2, carbono), atomo(M, O, oxigenio)), C\=C2, !.
---
+++ teste.pl
@@ -0,0 +1,36 @@
+%Átomos da Molécula m1
+atomo(m1,h1,hidrogenio).
+atomo(m1,h2,hidrogenio).
+atomo(m1,h3,hidrogenio).
+atomo(m1,h4,hidrogenio).
+atomo(m1,h5,hidrogenio).
+atomo(m1,h6,hidrogenio).
+atomo(m1,h7,hidrogenio).
+atomo(m1,c1,carbono).
+atomo(m1,c2,carbono).
+atomo(m1,c3,carbono).
+atomo(m1,c4,carbono).
+atomo(m1,c5,carbono).
+atomo(m1,c6,carbono).
+atomo(m1,c7,carbono).
+atomo(m1,ch,cloro).
+
+%Ligações Químicas da Molécula m1
+ligacaosimples(m1,h1,c1).
+ligacaosimples(m1,h2,c3).
+ligacaosimples(m1,h3,c3).
+ligacaosimples(m1,h4,c5).
+ligacaosimples(m1,h5,c3).
+ligacaosimples(m1,h6,c6).
+ligacaosimples(m1,h7,c7).
+ligacaosimples(m1,c1,c2).
+ligacaosimples(m1,c2,ch).
+ligacaosimples(m1,c3,c4).
+ligacaosimples(m1,c4,c6).
+ligacaosimples(m1,c5,c7).
+
+ligacaodupla(m1,c6,c7).
+ligacaodupla(m1,c1,c4).
+ligacaodupla(m1,c2,c5).
+
+
